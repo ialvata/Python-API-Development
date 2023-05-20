76405e0f4ce8216e7b5421e30effb41d51ba89b0
I T <ivo.a.v.tavares@gmail.com>
2023-05-20 09:19:40 +0100

    Added status codes, and http exceptions, and delete method. I would have divided this into smaller commits...

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Module Docstring
 from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 
 
@@ -44,16 +44,24 @@ def get_all_posts():
     return {"data": myposts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(payload: Post):
     """
     function docstring
     """
     incoming_post = payload.dict()
-    incoming_post["id"] = len(myposts)
+    incoming_post["id"] = myposts[len(myposts) - 1]["id"] + 1
     myposts.append(incoming_post)
     print(f"Length of myposts: {incoming_post['id']}")
-    return post
+    return incoming_post
+
+
+@app.get("/posts/latest")
+def get_latest_post():
+    """
+    function docstring
+    """
+    return {"latest": myposts[len(myposts) - 1]}
 
 
 @app.get("/posts/{identifier}")
@@ -63,7 +71,23 @@ def get_post(identifier: int):
     """
     post_wanted = [post for post in myposts if post["id"] == identifier]
     print(post_wanted)
-    return {"fetched_post": post_wanted}
+    if post_wanted == []:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id {identifier} not found!",
+        )
+    # response.status_code = status.HTTP_404_NOT_FOUND
+    return {"fetched_post": post_wanted[0]}
+
+
+@app.delete("/posts/{identifier}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(identifier: int):
+    """
+    function docstring
+    """
+    post_wanted = [post for post in myposts if post["id"] == identifier]
+    if post_wanted != []:
+        myposts.remove(post_wanted[0])
 
 
 if __name__ == "__main__":
