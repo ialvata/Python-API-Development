9a5458586ebd37c1b841e44f2303c0a5a666dacb
I T <ivo.a.v.tavares@gmail.com>
2023-06-12 19:42:05 +0100

    Prometheus is working. Dashboard must be imported manually.

--- app/main.py
+++ app/main.py
@@ -8,9 +8,10 @@ from fastapi import FastAPI
 from app.routers import auth, posts, users
 from db import schemas
 from db.db_orm import Base, database_gen, engine
-from db.db_utils import init_db  # , stream_mocker
+from db.db_utils import init_db, stream_mocker
 from db.repository import PostgresDB
 from grafana.grafana_utils import Grafana
+from prometheus.prometheus_utils import PrometheusDB
 
 #####################    Creating some initial data in Postgres db    #######################
 print(f"Creating tables in {Base}")
@@ -26,10 +27,12 @@ grafana.create_api_key()
 postgres_db = PostgresDB(filename="./db/database.ini", section="postgresql")
 grafana.add_database_source(postgres_db)
 grafana.upload_to_grafana(postgres_db)
+prometheus_db = PrometheusDB(filename="./prometheus/config.ini", section="prometheus")
 
-
+grafana.add_prometheus_source(prometheus_db)
+print(grafana.get_all_datasources_info())
 #####################      Simulating a stream of posts    ##################################
-# stream_mocker(get_initial_db)
+stream_mocker(get_initial_db)
 
 ##############################    Creatng FastAPI App   ##########################
 app = FastAPI()
