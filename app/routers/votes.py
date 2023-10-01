3122389ef89d4425822280c6c8a8bc51fe56587b
I T <ivo.a.v.tavares@gmail.com>
2023-10-01 13:40:34 +0100

    created votes router

---
+++ app/routers/votes.py
@@ -0,0 +1,87 @@
+"""
+Module responsible for Posts related operations
+"""
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from authetication.oauth2 import TokenData, get_current_user
+from db import schemas
+from db.db_orm import database_gen
+from pydantic_models.votes import VoteInput, VoteResponse
+
+#################################        Router        ################################
+router = APIRouter(prefix="/votes", tags=["Votes"])
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def vote(
+    vote: VoteInput,
+    current_user: schemas.User = Depends(get_current_user),
+    db_session: Session = Depends(database_gen),
+    token_data: TokenData = Depends(get_current_user),
+):
+    """
+    This creates a row in the vote table.
+
+    Parameters
+    ----------
+    current_user:.
+    """
+    vote_query = db_session.query(schemas.Vote).filter(
+        schemas.Vote.post_id == vote.post_id, schemas.Vote.user_id == current_user.id
+    )
+
+    found_vote = vote_query.first()
+    if vote.dir == 1:
+        if found_vote:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"user {current_user.id} has already voted on post {vote.post_id}",
+            )
+        new_vote = schemas.Vote(
+            post_id=vote.post_id,
+            user_id=current_user.id,
+            username=current_user.username,
+            rating=vote.rating,
+            published=vote.published,
+        )
+        db_session.add(new_vote)
+        db_session.commit()
+        return {"message": "successfully added vote"}
+
+    else:
+        if not found_vote:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Vote does not exist"
+            )
+        vote_query.delete(synchronize_session=False)
+        db_session.commit()
+        return {"message": "successfully deleted vote"}
+
+
+@router.get("/", status_code=status.HTTP_200_OK, response_model=list[VoteResponse])
+def get_votes(
+    username: str,
+    num_votes: int = 10,
+    skip: int = 0,
+    db_session: Session = Depends(database_gen),
+    token_data: TokenData = Depends(get_current_user),
+):
+    """
+    restric_user: If True, then we only show posts for the user that's logged in.
+    num_posts: number of posts to show.
+
+    To implement pagination in results, we should use .offset(num_pag) in the query below,
+    where the results thrown will be offsetted by num_pag:int.
+    """
+
+    votes = (
+        db_session.query(schemas.Vote)
+        .where(
+            schemas.Vote.username == username,
+        )
+        .offset(skip)
+        .limit(num_votes)
+        .all()
+    )
+    return votes
