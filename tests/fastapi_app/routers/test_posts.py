a79a1e60ddf8af50c697377baaf455c6399a48a4
I T <ivo.a.v.tavares@gmail.com>
2023-10-22 12:42:53 +0100

    test_create_post

--- tests/fastapi_app/routers/test_posts.py
+++ tests/fastapi_app/routers/test_posts.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from db.schemas import Post as Post_SQL
@@ -26,38 +27,57 @@ def test_unauthorized_user_get_one_post(mock_client: TestClient, mock_posts: lis
     assert res.status_code == 401
 
 
-# def test_get_one_post_not_exist(authorized_client, test_posts):
-#     res = authorized_client.get(f"/posts/88888")
-#     assert res.status_code == 404
+def test_get_one_post_not_exist(
+    mock_authorized_client: TestClient, mock_posts: list[Post_SQL]
+):
+    res = mock_authorized_client.get("/posts/88888")
+    assert res.status_code == 404
 
 
-# def test_get_one_post(authorized_client, test_posts):
-#     res = authorized_client.get(f"/posts/{test_posts[0].id}")
-#     post = schemas.PostOut(**res.json())
-#     assert post.Post.id == test_posts[0].id
-#     assert post.Post.content == test_posts[0].content
-#     assert post.Post.title == test_posts[0].title
+def test_get_one_post(mock_authorized_client: TestClient, mock_posts: list[Post_SQL]):
+    res = mock_authorized_client.get(f"/posts/{mock_posts[0].id}")
+    post = PostResponse(**res.json())
+    assert post.id == mock_posts[0].id
+    assert post.content == mock_posts[0].content
+    assert post.title == mock_posts[0].title
 
 
-# @pytest.mark.parametrize("title, content, published", [
-#     ("awesome new title", "awesome new content", True),
-#     ("favorite pizza", "i love pepperoni", False),
-#     ("tallest skyscrapers", "wahoo", True),
-# ])
-# def test_create_post(authorized_client, test_user, test_posts, title, content, published):
-#     res = authorized_client.post(
-#         "/posts/", json={"title": title, "content": content, "published": published})
+@pytest.mark.parametrize(
+    "title, content, published",
+    [
+        ("awesome new title", "awesome new content", True),
+        ("favorite pizza", "i love pepperoni", False),
+        ("tallest skyscrapers", "wahoo", True),
+    ],
+)
+def test_create_post(
+    mock_authorized_client: TestClient,
+    mock_user,
+    mock_posts: list[Post_SQL],
+    title,
+    content,
+    published,
+):
+    res = mock_authorized_client.post(
+        "/posts/",
+        json={
+            "title": title,
+            "content": content,
+            "published": published,
+            "username": mock_user["username"],
+        },
+    )
 
-#     created_post = schemas.Post(**res.json())
-#     assert res.status_code == 201
-#     assert created_post.title == title
-#     assert created_post.content == content
-#     assert created_post.published == published
-#     assert created_post.owner_id == test_user['id']
+    created_post = PostResponse(**res.json())
+    assert res.status_code == 201
+    assert created_post.title == title
+    assert created_post.content == content
+    assert created_post.published == published
+    assert created_post.user.id == mock_user["id"]
 
 
-# def test_create_post_default_published_true(authorized_client, test_user, test_posts):
-#     res = authorized_client.post(
+# def test_create_post_default_published_true(mock_authorized_client, test_user, mock_posts):
+#     res = mock_authorized_client.post(
 #         "/posts/", json={"title": "arbitrary title", "content": "aasdfjasdf"})
 
 #     created_post = schemas.Post(**res.json())
@@ -68,77 +88,77 @@ def test_unauthorized_user_get_one_post(mock_client: TestClient, mock_posts: lis
 #     assert created_post.owner_id == test_user['id']
 
 
-# def test_unauthorized_user_create_post(client, test_user, test_posts):
+# def test_unauthorized_user_create_post(client, test_user, mock_posts):
 #     res = client.post(
 #         "/posts/", json={"title": "arbitrary title", "content": "aasdfjasdf"})
 #     assert res.status_code == 401
 
 
-# def test_unauthorized_user_delete_Post(client, test_user, test_posts):
+# def test_unauthorized_user_delete_Post(client, test_user, mock_posts):
 #     res = client.delete(
-#         f"/posts/{test_posts[0].id}")
+#         f"/posts/{mock_posts[0].id}")
 #     assert res.status_code == 401
 
 
-# def test_delete_post_success(authorized_client, test_user, test_posts):
-#     res = authorized_client.delete(
-#         f"/posts/{test_posts[0].id}")
+# def test_delete_post_success(mock_authorized_client, test_user, mock_posts):
+#     res = mock_authorized_client.delete(
+#         f"/posts/{mock_posts[0].id}")
 
 #     assert res.status_code == 204
 
 
-# def test_delete_post_non_exist(authorized_client, test_user, test_posts):
-#     res = authorized_client.delete(
+# def test_delete_post_non_exist(mock_authorized_client, test_user, mock_posts):
+#     res = mock_authorized_client.delete(
 #         f"/posts/8000000")
 
 #     assert res.status_code == 404
 
 
-# def test_delete_other_user_post(authorized_client, test_user, test_posts):
-#     res = authorized_client.delete(
-#         f"/posts/{test_posts[3].id}")
+# def test_delete_other_user_post(mock_authorized_client, test_user, mock_posts):
+#     res = mock_authorized_client.delete(
+#         f"/posts/{mock_posts[3].id}")
 #     assert res.status_code == 403
 
 
-# def test_update_post(authorized_client, test_user, test_posts):
+# def test_update_post(mock_authorized_client, test_user, mock_posts):
 #     data = {
 #         "title": "updated title",
 #         "content": "updatd content",
-#         "id": test_posts[0].id
+#         "id": mock_posts[0].id
 
 #     }
-#     res = authorized_client.put(f"/posts/{test_posts[0].id}", json=data)
+#     res = mock_authorized_client.put(f"/posts/{mock_posts[0].id}", json=data)
 #     updated_post = schemas.Post(**res.json())
 #     assert res.status_code == 200
 #     assert updated_post.title == data['title']
 #     assert updated_post.content == data['content']
 
 
-# def test_update_other_user_post(authorized_client, test_user, test_user2, test_posts):
+# def test_update_other_user_post(mock_authorized_client, test_user, test_user2, mock_posts):
 #     data = {
 #         "title": "updated title",
 #         "content": "updatd content",
-#         "id": test_posts[3].id
+#         "id": mock_posts[3].id
 
 #     }
-#     res = authorized_client.put(f"/posts/{test_posts[3].id}", json=data)
+#     res = mock_authorized_client.put(f"/posts/{mock_posts[3].id}", json=data)
 #     assert res.status_code == 403
 
 
-# def test_unauthorized_user_update_post(client, test_user, test_posts):
+# def test_unauthorized_user_update_post(client, test_user, mock_posts):
 #     res = client.put(
-#         f"/posts/{test_posts[0].id}")
+#         f"/posts/{mock_posts[0].id}")
 #     assert res.status_code == 401
 
 
-# def test_update_post_non_exist(authorized_client, test_user, test_posts):
+# def test_update_post_non_exist(mock_authorized_client, test_user, mock_posts):
 #     data = {
 #         "title": "updated title",
 #         "content": "updatd content",
-#         "id": test_posts[3].id
+#         "id": mock_posts[3].id
 
 #     }
-#     res = authorized_client.put(
+#     res = mock_authorized_client.put(
 #         f"/posts/8000000", json=data)
 
 #     assert res.status_code == 404
