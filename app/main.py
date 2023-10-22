7e3dc53ff57e1926124136b452bafd77da35da84
I T <ivo.a.v.tavares@gmail.com>
2023-10-22 10:42:11 +0100

    separated adding integrations.

--- app/main.py
+++ app/main.py
@@ -5,20 +5,6 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.routers import auth, posts, users, votes
-from db.repository import PostgresDB
-from grafana.grafana_utils import Grafana
-from prometheus.prometheus_utils import PrometheusDB
-
-#####################    Adding a Postgres db datasource to Grafana     #######################
-grafana = Grafana()
-grafana.create_api_key()
-postgres_db = PostgresDB(filename="./db/database.ini", section="postgresql")
-grafana.add_database_source(postgres_db)
-grafana.upload_to_grafana(postgres_db)
-#####################      Adding a Prometheus DB to Grafana      #############################
-prometheus_db = PrometheusDB(filename="./prometheus/config.ini", section="prometheus")
-grafana.add_prometheus_source(prometheus_db)
-# print(grafana.get_all_datasources_info())
 
 ##############################    Creatng FastAPI App   ##########################
 app = FastAPI()
